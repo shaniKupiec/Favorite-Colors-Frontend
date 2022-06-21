e9f29c3c82ea7c947213e2e111d345cee50f8f20
shani kupiec <shani.kupiec@gmail.com>
2022-06-21 09:59:40 +0300

    add onVote and useState in app cmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
+import { useEffect, useState } from "react";
 import { ColorList } from "./components/ColorList";
+import { Data } from "./models/data.models";
 
 function App() {
-  const data = [
-    {
-      color: "red",
-      votes: 20,
-    },
-    {
-      color: "green",
-      votes: 100,
-    },
-    {
-      color: "blue",
-      votes: 5,
-    },
-  ];
-  
+  const [dataBase, setDataBase] = useState<Data[]>([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const newData: Data[] = [
+      {
+        color: "red",
+        votes: 20,
+      },
+      {
+        color: "green",
+        votes: 100,
+      },
+      {
+        color: "blue",
+        votes: 5,
+      },
+    ];
+    setDataBase(newData);
+  };
+
   const onVote = (color: string) => {
-    console.log("clicked! color", color);
-    data.map((d) => {
-      if (d.color === color) d.votes++;
-    });
-    console.log('data',data)
+    setDataBase((dataBase) =>
+      dataBase.map((d) => {
+        if (d.color === color) d.votes++;
+        return d;
+      })
+    );
   };
 
   return (
     <section className="app">
       <div className="app__title">favorite colors</div>
-      <ColorList data={data} onVote={onVote} />
+      <ColorList dataBase={dataBase} onVote={onVote} />
     </section>
   );
 }
--- src/components/ColorList.tsx
+++ src/components/ColorList.tsx
@@ -1,10 +1,18 @@
+import { Data } from "../models/data.models";
 import { ColorPreview } from "./ColorPreview";
 
-export const ColorList = ({ data, onVote }: any) => {
+export const ColorList = ({ dataBase, onVote }: any) => {
+  console.log('dataBase',dataBase)
+  const votes = dataBase.map((data: Data) => {
+    return data.votes;
+  });
+  const maxWidth = Math.max(...votes);
+  console.log('maxWidth',maxWidth)
+
   return (
     <section className="color-list">
-      {data.map((cmpData: any) => (
-        <ColorPreview key={cmpData.color} color={cmpData.color} votes={cmpData.votes} onVote={onVote} maxWidth={100} />
+      {dataBase.map((data: Data) => (
+        <ColorPreview key={data.color} color={data.color} votes={data.votes} onVote={onVote} maxWidth={maxWidth} />
       ))}
     </section>
   );
